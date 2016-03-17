f33c21dbf106fb83fcb09225f6b5d608b271fc8d
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-03-17 01:24:31 -0400

    Update shiny-server.sh

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ### In rserver.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
+# `chpst -u xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
 
-exec start-stop-daemon --start --quiet --pidfile /var/run/shiny-server.pid --make-pidfile  --exec /usr/bin/shiny-server >>/var/log/shiny-server.log 2>&1
+exec chpst -u shiny /usr/bin/shiny-server  2>&1
