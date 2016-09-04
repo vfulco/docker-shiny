66965659e9c63b8668102d38ce1b0bdc0e3988b2
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-09-03 22:50:32 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
       #code that need to run only one time ....
-        sed -i '114 a <h2><a href="./examples/">Other examples of Shiny application</a> </h2>' /srv/shiny-server/index.html
+        
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
