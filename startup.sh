a904b8ff32455474e862f1393e756dfb4328d979
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-06-17 15:13:18 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,10 +5,12 @@ set -e
 
 if [ -f /etc/configured ]; then
         chown -R shiny:shiny /srv/shiny-server
+        chown -R shiny:shiny /var/lib/shiny-server
         echo 'already configured'
 else
       #code that need to run only one time ....
       chown -R shiny:shiny /srv/shiny-server
+      chown -R shiny:shiny /var/lib/shiny-server
         
         #needed for fix problem with ubuntu and cron
         update-locale 
